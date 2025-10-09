e67f89ca7f9ccfc4bfda3370ed8c13101ffa533a
César Vázquez <agua.ceavaz97@gmail.com>
2025-10-08 19:28:13 -0600

    Update Learn Directory

---
+++ 01.Learn/my_app/src/App.js
@@ -0,0 +1,13 @@
+import Ejemplo from './components/useEffectEjemplo.jsx';
+import Contador from './components/Contador.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Ejemplo/>
+      <Contador/>
+    </div>
+  );
+}
+
+export default App;
